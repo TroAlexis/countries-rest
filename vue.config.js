87e29838b08456913b187ff524a7779ae429351f
TroAlexis <aleksei.troshin1997@gmail.com>
2021-06-11 16:10:36 +0300

    Configured vue to use sass, installed stylelint, added scss media lib with reset.scss, prepared the project.

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/global/*.scss'),
+      ],
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      new StyleLintPlugin({
+        files: ['src/**/*.{vue,scss}'],
+      }),
+    ],
+  },
+};
