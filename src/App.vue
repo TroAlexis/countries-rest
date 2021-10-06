5702b379fe2e7bc82982b8b00b397f64755f25d4
Alex Troshin <a.troshin@idaproject.com>
2021-10-06 21:48:55 +0300

    feat: add router-view transition
    fix: add mode out-in to router-view transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <Header/>
-  <router-view/>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 <script>
 import Header from '@/components/Header.vue';
