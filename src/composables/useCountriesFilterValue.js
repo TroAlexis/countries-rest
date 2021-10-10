0b2c73d2fe8b6bb9ef035c52ac030e1aa43da241
Alex Troshin <a.troshin@idaproject.com>
2021-10-10 21:13:43 +0300

    refactor: transfer useCountriesFilters logic to useCountriesFilterValue

--- src/composables/useCountriesFilterValue.js
+++ src/composables/useCountriesFilterValue.js
@@ -1,11 +1,13 @@
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 
-export default function useCountriesFilterValue(filters, filterBy) {
+export default function useCountriesFilterValue(filter, filterBy) {
   const store = useStore();
+  const filters = computed(() => store.state.countries.filters);
+  store.dispatch('countries/updateFilter', filter);
 
   const filterValue = computed({
-    get: () => filters[filterBy]?.value,
+    get: () => filters.value[filterBy]?.value,
     set: (value) => store.dispatch('countries/updateFilter', {
       key: filterBy,
       value,
