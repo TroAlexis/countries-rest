9d24d92a8f00c45896ae9d3e6edb7d13f4084310
Alex Troshin <a.troshin@idaproject.com>
2021-10-30 18:14:25 +0300

    refactor(countrieslist): adapt to Composition API

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -34,10 +34,13 @@
 </template>
 
 <script>
-import { mapActions, mapGetters, mapState } from 'vuex';
+import {
+  useStore,
+} from 'vuex';
 import CountryCard from '@/components/CountryCard/CountryCard.vue';
 import Spinner from '@/components/TheSpinner.vue';
 import Pagination from '@/components/Pagination/Pagination.vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'CountriesList',
@@ -48,34 +51,28 @@ export default {
       default: 1,
     },
   },
-  data() {
-    return {
-      showMax: 12,
-    };
-  },
-  computed: {
-    ...mapState('countries', { loading: 'loading' }),
-    ...mapGetters('countries', ['filteredCountries']),
-    countriesLimited() {
-      return this.filteredCountries.slice(
-        (this.showMax * this.currentPage) - this.showMax,
-        this.showMax * this.currentPage,
-      );
-    },
-    getPages() {
-      return Math.ceil(this.filteredCountries.length / this.showMax);
-    },
-    hasCountriesStored() {
-      return this.filteredCountries.length;
-    },
-  },
-  methods: {
-    ...mapActions('countries', ['fetchCountries']),
-  },
-  created() {
-    if (!this.hasCountriesStored) {
-      this.fetchCountries();
+  setup(props) {
+    const store = useStore();
+    const showMax = ref(12);
+    const loading = computed(() => store.state.countries.loading);
+    const filteredCountries = computed(() => store.getters['countries/filteredCountries']);
+    const countriesLimited = computed(() => filteredCountries.value.slice(
+      (showMax.value * props.currentPage) - showMax.value,
+      showMax.value * props.currentPage,
+    ));
+    const getPages = computed(() => Math.ceil(filteredCountries.value.length / showMax.value));
+    const hasCountriesStored = computed(() => !!filteredCountries.value.length);
+
+    if (!hasCountriesStored.value) {
+      store.dispatch('countries/fetchCountries');
     }
+
+    return {
+      showMax,
+      loading,
+      countriesLimited,
+      getPages,
+    };
   },
 };
 </script>
